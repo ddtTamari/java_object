1529c3160a4dab65b29f6a85448a6254f1795fcb
Owner-THINK\Owner <ddt.jissyusei5@gmail.com>
2018-08-22 21:00:41 +0900

    課題番号 work3-2
    当日行った内容  100％

---
+++ Player.java
@@ -0,0 +1,85 @@
+/*
+  演習3-2 Lesson2で作成したSimpleJanken.javaを参考にして、
+          リスト3-1の各メソッドの中身(①～③の部分)を作成し、Playerクラスを完成させてください。
+  演習日  8月22日
+  製作者  玉利仁美
+ */
+package work3_2;
+
+//プレイヤークラス
+public class Player {
+
+    //ジャンケンで出す手を定数で宣言する
+    public static final int STONE = 0;
+    //ジャンケンで出す手を定数で宣言する
+    public static final int SCISSORS = 1;
+    //ジャンケンで出す手を定数で宣言する
+    public static final int PAPER = 2;
+
+    //プレイヤーの名前を定義する
+    private String name_;
+
+    //プレイヤーの勝った数を保持するための変数
+    private int winCont_ = 0;
+
+    /**
+     * プレイヤーの手を決めるメソッド
+     * @author 玉利
+     * @return プレイヤーの手
+     */
+    //プレイヤーの手を決め返すメソッド
+    public int showHand() {
+        //プレイヤーの手を保持するための変数
+        int playerHand = 0;
+
+        //0から3までのランダムな変数を出すための変数
+        double randomNum = Math.random() * 3;
+        //ランダムな手の値が1未満ならPlayer1の出す手はグーにする
+        if (randomNum < 1) {
+            //プレイヤー1の手はグー
+            playerHand = STONE;
+            //プレイヤー1がグーだったことを明示する
+            System.out.print("グー");
+            //ランダムで出た値が1以上2未満なら出す手はチョキになる
+        } else if (randomNum < 2) {
+            //プレイヤー1の手はチョキ
+            playerHand = SCISSORS;
+            //プレイヤー1がチョキだったことを明示する
+            System.out.print("チョキ");
+            //ランダムで出た値が2以上3未満なら出す手はパーになる
+        } else if (randomNum < 3) {
+            //プレイヤー1の手はチョキ
+            playerHand = PAPER;
+            //プレイヤー1がパーだったことを明示する
+            System.out.print("パー");
+        }
+        //ランダムに出た手を返す
+        return playerHand;
+    }
+
+    /**
+     * プレイヤーの勝利数を保持するためのメソッド
+     * @author 玉利
+     * @param result true:勝ち,false:負け
+     */
+    //審判からもらった勝利判定をもとに勝利数を計算するメソッド
+    public void notifyResult(boolean result) {
+        //審判からの勝敗が勝ちならば
+        if (result == true) {
+            //勝利数に1を追加する
+            winCont_ = winCont_ + 1;
+        }
+    }
+
+    /**
+     * 審判が勝利数を取得できるようにするメソッド
+     * @author 玉利
+     * @return プレイヤーの勝利数
+     */
+    //審判が最終判定に使う結果を出すための値を返す
+    public int getWinCount() {
+        //カウントされた勝利数を返す
+        return winCont_;
+    }
+
+}
